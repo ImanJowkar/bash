7e7faa70757f3cd6fb55e48b47b528baa4b1c07b
ImanJowkar <77024269+ImanJowkar@users.noreply.github.com>
2024-06-01 00:42:46 +0330

    add default value to bash

--- 1-basic/4-app.sh
+++ 1-basic/4-app.sh
@@ -4,6 +4,7 @@
 
 echo $0   # is the name of the script itself (script.sh)
 echo ${1:-default_value}   # is the first positional argument (filename1)
+echo ${1:--100}   # is the first positional argument (filename1)
 echo $2   # is the second positional argument 
 echo $3   # is the last argument of the script
 echo $9   # would be the ninthe argument and ${10} the tenth
